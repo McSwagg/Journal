5acd6686facb16613d3a92d00fbf400ba89981e8
Nathan Wawro <nathanwawro@gmail.com>
2021-09-09 07:08:43 -0600

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Nathan Wawro on 9/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
